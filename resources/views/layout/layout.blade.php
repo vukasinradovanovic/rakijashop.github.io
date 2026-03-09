68b80599694b0e055e0c7d1c3d7e13d2fefd8d93
Vukasin Radovanovic <vukasinradovanovic@gmail.com>
2026-03-09 14:22:13 +0100

    Sass library optimized

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -13,7 +13,7 @@
         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Playfair+Display:wght@400;600;700&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    @vite(['resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
 </head>
 
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true,
+                silenceDeprecations: ['import', 'global-builtin', 'color-functions', 'slash-div', 'if-function'],
+            },
+        },
+    },
     plugins: [
         laravel({
             input: [
+                'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/js/script.js',
                 'resources/js/dashboard/dashboardScript.js',
